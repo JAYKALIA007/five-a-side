4d2d96aafce6cafd58cadb52bc3e0cfb7e840692
Jay Prakash Kalia <jaykalia047@gmail.com>
2024-02-28 01:44:58 +0530

    fix size of confetti

--- src/components/animations/ConfettiAnimation.tsx
+++ src/components/animations/ConfettiAnimation.tsx
@@ -3,7 +3,7 @@ import confetti from "../../assets/lotties/confetti.json";
 export const ConfettiAnimation = () => {
   return (
     <Lottie
-      style={{ height: "100%", width: "100%" }}
+      style={{ height: 120, width: 250 }}
       animationData={confetti}
       loop={false}
       autoPlay={true}
